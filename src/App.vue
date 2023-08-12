e5b3f93e1db653da59fa316c1fdc3983c56d7815
chiefJurist <anthonynnanna004@gmail.com>
2023-08-12 04:15:55 +0100

    Another commit

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,9 @@
         color: #2c3e50;
         margin-top: 60px;
     }
+    h1{
+        border-bottom: 1px solid #ddd;
+        display: inline-block;
+        padding-bottom: 10px;
+    }
 </style>
