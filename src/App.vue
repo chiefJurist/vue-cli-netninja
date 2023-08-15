1cd1b9cd453f14af197d5003fd70569ae0066531
chiefJurist <anthonynnanna004@gmail.com>
2023-08-15 13:11:38 +0100

    Slots part two

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <p>Welcome...</p>
     <div v-if="showModal">
         <Modal theme="sale" @close="toggleModal">
+            <template v-slot:links>
+                <a href="">Sign up now!</a>
+                <a href="">more info</a>
+            </template>
             <h1>Sign up for the Giveaway!</h1>
             <p>Grab your ninja swag for half price!</p>
         </Modal>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="backdrop" @click.self="closeModal">
         <div class="modal" :class="{sale: theme==='sale'}">
-            <slot></slot>
+            <slot>Default content</slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
@@ -37,14 +40,29 @@
         border: none;
         padding: 0;
     }
-    p{
+    .modal p{
         font-style: normal;
+    }
+    .modal .actions {
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+    .modal .actions a{
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
     }
     .modal.sale{
         background: crimson;
         color: white;
     }
     .modal.sale h1{
+        color: white;
+    }
+    .modal.sale .actions a{
         color: white;
     }
 </style>
